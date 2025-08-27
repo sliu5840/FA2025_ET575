d53fb42ac92d9a09ad130e31c036b2c9c33b9286
Susan Liu <susan.liu53@student.qcc.cuny.edu>
2025-08-27 19:53:34 +0000

    END OF CLASS

---
+++ Lab1_LIU.cpp
@@ -0,0 +1,34 @@
+/*
+Susan Liu
+Aug 27th, 2025
+LAB1: iostream
+*/
+// C++ library
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"------ Example 1: cout data ------"<<endl;
+    cout <<"This is a C++ code."<<endl;
+    //variables: temporary storage of data
+    // declare the variable first
+    string username = "peterpan123";
+    cout<<"\nWelcome to C++ \t\t"<<username<<endl;
+    
+    cout<<"------ Example 2: cin data ------"<<endl;
+    cout<<"Enter a username"<<endl;
+    cin >> username;
+    cout<<"New user is "<<username<<endl;
+    
+    cout<<"------ Example 3: data type ------"<<endl;
+    //declare variable
+    char sym = '%';
+    float distance = 5.63;
+    int number = 8.9;
+
+    cout<<"------ EXERCISE ------"<<endl;
+    //DECLARE VARIABLES
+    
+    return 0;
+}
