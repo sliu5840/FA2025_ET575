b0582f02ce6b3838d9ed1b1c2c7255e5d5b22b9b
Susan Liu <susan.liu53@student.qcc.cuny.edu>
2025-08-31 00:18:57 +0000

    FINISHED TESTER

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string n;
+    cout<<"\v ---------------- Welcome to C++ ----------------\n"<<endl;
+    cout<<"Enter your full name: ";
+    cin>>n;
+    cout<<"Welcome to C++ "<<n<<endl;
+    return 0;
+}
