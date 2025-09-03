a6a61ff5fafebe31c3c63d19025df857858b5bd5
Susan Liu <susan.liu53@student.qcc.cuny.edu>
2025-09-03 19:01:28 +0000

    NUMBER 2 IN CLASS

--- LAB 2: Operators/Lab2_LIU.cpp
+++ LAB 2: Operators/Lab2_LIU.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 int main()
 {
+    /*
+    
     cout<< "------- Example 1: Arithmetic Operators -------" << endl;
     //set number format
     cout.setf(ios::fixed);
@@ -24,6 +26,25 @@ int main()
     height = 0.5 * GRAVITY * time * time;
     //print results
     cout << "The height of the free fall object is " <<height<< " meters." << endl;
+    cout<<""<< endl;
+    
+    */
+    cout <<"------- Example 2: Assignment Operators -------" << endl;
+    int number1 = 10, number2 = 3;
+    cout<< "Original number1 = " <<number1<< endl;
+    number1 ++;
+    cout<< "Updated number1 = " <<number1<< endl;
+    cout<<"-----------------------------------------------"<<endl;
+    cout<< "Original number2 = " <<number2<< endl;
+    number2 --;
+    cout<< "Updated number2 = " <<number2<< endl;
+    number2 +=5;
+    cout<< "Updated number2 = " <<number2<< endl;
+    number1*=2;
+    cout<< "Updated number1 = " <<number1<< endl;
+    cout<<""<< endl;
+
+
     cout<<""<< endl;
     return 0;
 }
