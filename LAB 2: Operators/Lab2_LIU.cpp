6dddeae4dbbf128f43744d7124d3d738e07352fb
Susan Liu <susan.liu53@student.qcc.cuny.edu>
2025-09-03 19:50:14 +0000

    number 5 in class

--- LAB 2: Operators/Lab2_LIU.cpp
+++ LAB 2: Operators/Lab2_LIU.cpp
@@ -27,7 +27,7 @@ int main()
     //print results
     cout << "The height of the free fall object is " <<height<< " meters." << endl;
     cout<<""<< endl;
-    */
+    
 
     cout <<"------- Example 2: Assignment Operators -------" << endl;
     int number1 = 10, number2 = 3;
@@ -35,7 +35,6 @@ int main()
     number1 ++;
     cout<< "Updated number1 = " <<number1<< endl;
     cout<<"-----------------------------------------------"<<endl;
-
     cout<< "Original number2 = " <<number2<< endl;
     number2 --;
     cout<< "Updated number2 = " <<number2<< endl;
@@ -44,18 +43,19 @@ int main()
     number1*=2;
     cout<< "Updated number1 = " <<number1<< endl;
     cout<<"-----------------------------------------------"<<endl;
-
     cout<<"The value of number1 is " <<number1<< " and the value of number2 is " <<number2<< endl; 
     number1 %= number2;
     cout<< "After %= operation, the value of number1 is " <<number1<< endl;
     cout<< "After %= operation, the value of number2 is " <<number2<< endl;
     cout<<""<< endl;
     
+
     cout <<"------- Example 3: Comparison Operators -------" << endl;
     cout<<"Is number 1 equal to number2? " << (number1 == number2) << endl;
     cout<<"Is number1 greater than number2? "<< (number1 > number2)<<endl;
     cout<<"Is number1 less than number2? " <<(number1 < number2)<<endl;
     cout<<"Is number1 not equal to number2? " << (number1 != number2)<< endl;
+    cout <<""<< endl;
     
 
     cout <<"------- Example 4: Logic Operators -------" << endl;
@@ -69,6 +69,15 @@ int main()
     //number2 != 3 --> true;;;;;; number 2 > 10 --> false;;;;;; number2 < number1 --> false
     //true || false || false --> true
     cout<<"The result of check_or is "<<check_or<< endl;
+    cout <<""<< endl;
+    */
+
+    cout <<"------- Example 5: Casting -------" << endl;
+    int n = 5;
+    float m = n/2; //implicit casting
+    
+    cout <<"Implicit casting = "<<n/2<< endl;
+    cout <<"Implicit casting = "<<m<< endl;
 
     cout<<""<< endl;
     return 0;
