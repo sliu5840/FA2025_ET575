af713c454c42f79e4ae0e7c84b95e4a4c72e5f51
Susan Liu <susan.liu53@student.qcc.cuny.edu>
2025-10-26 03:05:48 +0000

    LAB12 HW

--- LAB 12:/Lab12_functions.cpp
+++ LAB 12:/Lab12_functions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <string>
 
 using namespace std;
@@ -7,4 +8,39 @@ int add(int num1, int num2){
     int sum = num1 + num2; 
     cout<<"The sum in the function add() is \t"<<sum<<endl;
     return sum;
+}
+
+//example 3: C++ Predefine Functions
+//function passes two arguments and returns the hypotenuse
+float hyp(float side1, float side2){
+    return sqrt(pow(side1, 2)+ pow(side2, 2));
+}
+
+//function to print result
+void printhyp(float s1, float s2, float h){
+    cout<<"the hypotenuse of side "<<s1<<" and side "<<s2<< " is "<<h<<"."<<endl;
+}
+//EXERCISE 
+/*
+
+FUNCTION TO COLLECT A # GREATER THAN 0
+
+FUNCTION TO CALCULATE THE DISTANCE OF FOUR POINT
+
+FUNCTION TO PRINT THE RESULT
+
+*/
+float posnum(float number){
+    while (number >0){
+        cout<<"Please enter a number greater than 0: "<<endl;
+        cin>>number;
+        return number;
+    }
+
+double dist(float x1, float x2, float y1, float y2){
+    return sqrt(pow(x2 - x1, 2) + pow(y2-y1), 2);
+}
+
+void printdist(float x1, float x2, float y1, float y2, float dist){
+    cout<<"The distance between point ("<<x1<<", "<<y1<<") and point (" <<x2<<", "<<y2<<") is "<<dist<<"."<<endl;
 }
--- LAB 12:/liu_lab12.cpp
+++ LAB 12:/liu_lab12.cpp
@@ -6,7 +6,15 @@ Date: Oct. 29th, 2025
 
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "Lab12_functions.cpp"
+
+int num = 8; //global variable
+void dosomething(){
+    //global variable num can be used and changed here
+    num += 10;
+    cout<<"The num in the function dosomething() = \t"<<num<<endl;
+}
 
 using namespace std;
 int main(){
@@ -16,17 +24,28 @@ int main(){
     cout<<""<<endl;
 
     cout<<"\n------------------Example 2: Global Variables ------------------"<<endl;
-    cout<<"Num original value is \t"<<num<<endl;
+    cout<<"num original value is \t"<<num<<endl; 
     dosomething();
     num = 5;
     cout<<"The num in the main() = \t"<<num<<endl;
     cout<<""<<endl;
 
     cout<<"\n------------------Example 3: Hypotenuse------------------"<<endl;
-    float s1 = 2;
-    float s2 = 3;
-    float h = hypotenuse(s1, s2);
+    float s1 = 3;
+    float s2 = 4;
+    float h = hyp(s1, s2);
     printhyp(s1, s2, h);
+    cout<<""<<endl;
+
+    cout<<"------------------LAB 12 EXERCISE------------------"<<endl;
+    //create functions to calculate distance between four points
+    float x1 = posnum(-7);
+    float y1 = posnum(2);
+    float x2 = posnum(4);
+    float y2 = posnum(3);
+
+    double distance = dist(x1, x2, y1, y2);
+    printdist(x1, x2, y1, y2, distance);
     cout<<""<<endl;
     return 0;
 }
